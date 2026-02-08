f1deda5120d1c12543f35dac21f9af69d27683bc
AntonBro338 <balesnui@mail.ru>
2026-02-08 15:09:46 +0700

    3.3 сделал

---
+++ chapter03/ex03.py
@@ -0,0 +1,26 @@
+print ("+----+----+")
+
+def do_twice (function, argyment):
+    function ()
+    function ()
+    
+def print_twice (text):
+    print (text)
+    print (text)
+    
+print_twice ("|    |    |")
+function = print_twice ("|    |    |")
+print ("+----+----+")
+
+def do_twice (function, argyment):
+    function ()
+    function ()
+    
+def print_twice (text):
+    print (text)
+    print (text)
+    
+print_twice ("|    |    |")
+function = print_twice ("|    |    |")
+
+print ("+----+----+")

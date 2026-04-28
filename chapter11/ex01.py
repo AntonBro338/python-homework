55ae8a7f85a84f5d0af74a0f54176b9ec61bc57d
AntonBro338 <balesnui@mail.ru>
2026-04-28 23:28:22 +0700

    Update ex01.py

--- chapter11/ex01.py
+++ chapter11/ex01.py
@@ -1,4 +1,10 @@
 def histogram(string: str) -> dict[str, int]:
+    """
+    >>> histogram ("Гиппопотамус")
+    {'Г': 1, 'и': 1, 'п': 3, 'о': 2, 'т': 1, 'а': 1, 'м': 1, 'у': 1, 'с': 1}
+    >>> histogram ("Бронтазавр")
+    {'Б': 1, 'р': 2, 'о': 1, 'н': 1, 'т': 1, 'а': 2, 'з': 1, 'в': 1}
+    """
     hist = dict()
     for char in string:
         hist[char] = hist.get(char, 0) + 1

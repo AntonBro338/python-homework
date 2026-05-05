6d4a5985b55dc1bb4b1b6a1161168c2aa6553d42
AntonBro338 <balesnui@mail.ru>
2026-05-05 20:13:21 +0700

    Вроде норм

---
+++ chapter13/ex01.py
@@ -0,0 +1,20 @@
+import string
+
+def read_file(filename: str) -> list[str]:
+    """Прочитать файл построчно в список"""
+    with open(filename) as file:
+        return [line.strip() for line in file]
+
+def parse_words(lines: list[str]) -> list[str]:
+    """Разбить список строк на слова и очистить их"""
+    all_words = []
+    for line in lines:
+        parts = line.split()
+        for word in parts:
+            word.lower()
+            word.strip(string.punctuation)
+            all_words.append(word)
+    return all_words
+
+
+print (parse_words(read_file('words.txt')))

5a6c4bb827141fe34e87aa865760bc9e4c1f4a2f
AntonBro338 <balesnui@mail.ru>
2026-05-07 18:34:13 +0700

    3

--- chapter13/ex03.py
+++ chapter13/ex03.py
@@ -1,27 +1,5 @@
 import string
-from ex02 import remove_metadata
-
-def read_file(filename: str) -> list[str]:
-    """Прочитать файл построчно в список"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        return [line.strip() for line in file]
-
-def parse_words(lines: list[str]) -> list[str]:
-    """Разбить список строк на слова"""
-    all_words = []
-    for line in lines:
-        parts = line.split()
-        for word in parts:
-            word.lower()
-            word.strip(string.punctuation)
-            all_words.append(word)
-    return all_words
-
-def histogram(words: list[str]) -> dict[str, int]:
-    word_list = {}
-    for word in words:
-        word_list[word] = word_list.get(word, 0) + 1
-    return word_list
+from lib import remove_metadata, histogram, parse_words, read_file
 
 def analyze_book(filename: str):
     """Проанализировать книгу и вывести статистику"""

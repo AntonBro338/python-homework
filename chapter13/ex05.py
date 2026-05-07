497f71bd1daa5aeae8d1f42d9a1b4b53d70d794c
AntonBro338 <balesnui@mail.ru>
2026-05-07 19:19:15 +0700

    5

--- chapter13/ex05.py
+++ chapter13/ex05.py
@@ -1,12 +1,16 @@
 import random
 from lib import histogram
 
-def choose_from_hist(hist: dict[str, int]) -> str:
+def choose_from_hist(hist: dict[str, int], n: int) -> str:
     pool = []
     for word, freq in hist.items():
         pool.extend([word] * freq)
-    for i in range(9):
-        print(random.choice(pool) if pool else "")
+    results = {word: 0 for word in hist.keys()}
+    for i in range(n):
+        results[random.choice(pool) if pool else ""] += 1
+    for word, count in results.items():
+        print(f"{word}: {count} раз ({count / n:.3%})")
     return random.choice(pool) if pool else ""
 
-choose_from_hist(histogram(["а", "а", "б"]))
+
+choose_from_hist(histogram(["а", "а", "б"]), 100000)

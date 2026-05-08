8c399cda1144ad83b03ccf2ebd338c01a9c3df33
AntonBro338 <balesnui@mail.ru>
2026-05-08 19:53:22 +0700

    1

--- chapter13/ex01.py
+++ chapter13/ex01.py
@@ -1,12 +1,18 @@
 import string
 
 def read_file(filename: str) -> list[str]:
-    """Прочитать файл построчно в список"""
+    r""" # я не знаю что это за r, но без нее он жалуется на кавычки
+    >>> read_file('words_2.txt')
+    ['book', '*** START OF THE PROJECT', 'look', 'dag', 'dog bag', '', 'bog', 'vog', 'z', 'gl hf', 'U2', '*** END OF THE PROJECT', 'gg', 'wp']
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         return [line.strip() for line in file]
 
 def parse_words(lines: list[str]) -> list[str]:
-    """Разбить список строк на слова"""
+    r"""
+    >>> parse_words (read_file('words_2.txt'))
+    ['book', '***', 'START', 'OF', 'THE', 'PROJECT', 'look', 'dag', 'dog', 'bag', 'bog', 'vog', 'z', 'gl', 'hf', 'U2', '***', 'END', 'OF', 'THE', 'PROJECT', 'gg', 'wp']
+    """
     all_words = []
     for line in lines:
         parts = line.split()
@@ -15,6 +21,3 @@ def parse_words(lines: list[str]) -> list[str]:
             word.strip(string.punctuation)
             all_words.append(word)
     return all_words
-
-
-print (parse_words(read_file('book.txt')))

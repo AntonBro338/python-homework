b143bbf4a2971e2e3a7eef68932a635aa12fc03d
AntonBro338 <balesnui@mail.ru>
2026-05-08 19:53:31 +0700

    2

--- chapter13/ex02.py
+++ chapter13/ex02.py
@@ -1,14 +1,10 @@
-import string
-
-
-def read_file(filename: str) -> list[str]:
-    """Прочитать файл построчно в список"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        return [line for line in file]
-
+from lib import read_file, parse_words
 
 def remove_metadata(lines: list[str]) -> list[str]:
-    """Удаляет строки до начала книги и после её завершения"""
+    r"""
+    >>> print (remove_metadata(read_file('words_2.txt')))
+    ['look', 'dag', 'dog bag', '', 'bog', 'vog', 'z', 'gl hf', 'U2']
+    """
     header_end = '*** START OF THE PROJECT'
     footer_start = '*** END OF THE PROJECT'
     start_idx = 0
@@ -19,18 +15,6 @@ def remove_metadata(lines: list[str]) -> list[str]:
         if line.startswith(footer_start):
             end_idx = i
     return lines[start_idx:end_idx]
-
-
-def parse_words(lines: list[str]) -> list[str]:
-    """Разбить список строк на слова"""
-    all_words = []
-    for line in lines:
-        parts = line.split()
-        for word in parts:
-            word = word.lower() #сли без word то работает, но тогда не приводит к одному регистру
-            word = word.strip(string.punctuation)
-            all_words.append(word)
-    return all_words
 
 print (parse_words(remove_metadata(read_file('book.txt'))))
 

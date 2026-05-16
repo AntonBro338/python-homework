267ae7c368ccae4db9a1d7aad760f45bad88a93b
AntonBro338 <balesnui@mail.ru>
2026-05-16 17:27:35 +0700

    8_3

--- smartiqa/ex08_3.py
+++ smartiqa/ex08_3.py
@@ -1,9 +1,11 @@
 from collections import Counter
 
 def top3(st: str) -> list[tuple[str, int]]:
-    return Counter(st.lower().replace(' ', '')).most_common(30)
+    """
+    >>> top3('')
+    []
+    >>> top3('Голова думала')
+    [('а', 3), ('о', 2), ('л', 2)]
 
-# Тесты
-print(top3('Улыбкой ясною природа Сквозь сон встречает утро года Синея блещут небеса. Еще прозрачные, леса'))
-print(top3(''))
-print(top3('Голова думала'))
+    """
+    return Counter(st.lower().replace(' ', '')).most_common(3)

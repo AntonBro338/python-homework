08ec976ab571157ea92e1be82d12c8cfdbb0730c
AntonBro338 <balesnui@mail.ru>
2026-07-18 19:45:32 +0700

    за 19 занятие часть 2, с доктестами

--- chapter 19/ex06.py
+++ chapter 19/ex06.py
@@ -1,6 +1,10 @@
 from collections import Counter
 
 def count_it(sequence: str) -> dict:
+    """"
+    >>> count_it ("12233344455555666666")
+    {6: 6, 5: 5, 3: 3}
+    >>> count_it ("")
+    {}
+    """
     return dict(Counter(int(num) for num in sequence).most_common(3))
-
-print (count_it ("1111111112345666666"))
--- chapter 19/ex07.py
+++ chapter 19/ex07.py
@@ -1,4 +1,10 @@
 def classify_words(words: list[str]) -> dict[str, list[str]]:
+    """
+    >>> classify_words (['EewwWrwrwrw', 'afhgfhfhh'])
+    {'e': ['EewwWrwrwrw'], 'a': ['afhgfhfhh']}
+    >>> classify_words ([])
+    {}
+    """
     result = {}
     for word in words:
         if word:
@@ -7,5 +13,3 @@ def classify_words(words: list[str]) -> dict[str, list[str]]:
                 result[first_letter] = []
             result[first_letter].append(word)
     return result
-
-print (classify_words (['EewwWrwrwrw', 'afhgfhfhh']))
